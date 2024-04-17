bc37517f8454af0043873deba1af79f6c8b5d727
Arnab <grinit10@gmail.com>
2024-04-17 13:38:32 +1000

    Finishing up with deps.

--- main.py
+++ main.py
@@ -1,24 +1,66 @@
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+from http.client import HTTPException
+from typing import Annotated
 
+from fastapi import Cookie, Depends, FastAPI, Header
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-    
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+async def verify_token(x_token: Annotated[str, Header()]):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: Annotated[str, Header()]):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+def query_extractor(q: str | None = None):
+    return q
+
+
+def query_or_cookie_extractor(
+    q: Annotated[str, Depends(query_extractor)],
+    last_query: Annotated[str | None, Cookie()] = None,
+):
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/sub-dependencies/")
+async def sub_dependencies(
+    query_or_default: Annotated[str, Depends(query_or_cookie_extractor)],
+):
+    return {"q_or_cookie": query_or_default}
+
+@app.get("/path-dependencies/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def path_dependencies():
+    return [{"item": "Foo"}, {"item": "Bar"}]
+
+@app.get("/items/")
+# Annotated[CommonQueryParams, Depends(CommonQueryParams)]) is same as Annotated[CommonQueryParams, Depends()]
+async def read_items(commons: Annotated[CommonQueryParams, Depends()]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
+
+@app.get("/users/")
+async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
